f350412dccf4f2bc57c256a6a05bec4cb36c6f22
AlexYariven <sander2002@live.se>
2020-02-14 10:11:15 +0100

    Add project files.

---
+++ CAM/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace CAM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.START = new System.Windows.Forms.Button();
+            this.Reset = new System.Windows.Forms.Button();
+            this.Capture = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.Pause = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // START
+            // 
+            this.START.Location = new System.Drawing.Point(223, 381);
+            this.START.Name = "START";
+            this.START.Size = new System.Drawing.Size(355, 57);
+            this.START.TabIndex = 0;
+            this.START.Text = "START";
+            this.START.UseVisualStyleBackColor = true;
+            this.START.Click += new System.EventHandler(this.START_Click);
+            // 
+            // Reset
+            // 
+            this.Reset.Location = new System.Drawing.Point(118, 410);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(99, 28);
+            this.Reset.TabIndex = 1;
+            this.Reset.Text = "Reset";
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // Capture
+            // 
+            this.Capture.Location = new System.Drawing.Point(689, 410);
+            this.Capture.Name = "Capture";
+            this.Capture.Size = new System.Drawing.Size(99, 28);
+            this.Capture.TabIndex = 2;
+            this.Capture.Text = "Capture";
+            this.Capture.UseVisualStyleBackColor = true;
+            this.Capture.Click += new System.EventHandler(this.Capture_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(12, 410);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(100, 28);
+            this.Exit.TabIndex = 3;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // Pause
+            // 
+            this.Pause.Location = new System.Drawing.Point(584, 410);
+            this.Pause.Name = "Pause";
+            this.Pause.Size = new System.Drawing.Size(99, 28);
+            this.Pause.TabIndex = 4;
+            this.Pause.Text = "Pause";
+            this.Pause.UseVisualStyleBackColor = true;
+            this.Pause.Click += new System.EventHandler(this.Pause_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(190, 24);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 44);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(775, 331);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Pause);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.Capture);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.START);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button START;
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.Button Capture;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Button Pause;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ CAM/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace CAM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private FilterInfoCollection CaptureDevices;
+        private VideoCaptureDevice videoSource;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CaptureDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo Device in CaptureDevices)
+            {
+                comboBox1.Items.Add(Device.Name);
+            }
+            comboBox1.SelectedIndex = 0;
+            videoSource = new VideoCaptureDevice();
+
+            string connectionString;
+            SqlConnection con;
+            connectionString = @"";
+            con = new SqlConnection(connectionString);
+            con.Open();
+
+            SqlCommand command;
+            sqlDataReader dataReader;
+            string sql, output = "";
+            sql = "Select texts from texts";
+            command = new SqlCommand(sql, con);
+            dataReader = command.ExecuteReader();
+
+            dataReader.Read();
+            button1.Text = "tryck inte";
+            con.Close();
+
+        }
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            if (videoSource.IsRunning==true)
+            {
+                videoSource.Stop();
+            }
+            Application.Exit(null);
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            videoSource.Stop();
+            pictureBox1.Image = null;
+            pictureBox1.Invalidate();
+            
+        }
+
+        private void START_Click(object sender, EventArgs e)
+        {
+            videoSource = new VideoCaptureDevice(CaptureDevices[comboBox1.SelectedIndex].MonikerString);
+            videoSource.NewFrame += new NewFrameEventHandler(VideoSource_NewFrame);
+        }
+
+        private void VideoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        private void Pause_Click(object sender, EventArgs e)
+        {
+            videoSource.Stop();
+        }
+
+        private void Capture_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = (Bitmap)pictureBox1.Image.Clone();
+        }
+
+       
+
+    }
+}
